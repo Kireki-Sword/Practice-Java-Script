0edabd404013a80ea660010fdf305517abc9602f
Kireki-Sword <286178854+Kireki-Sword@users.noreply.github.com>
2026-06-15 18:41:22 +0000

    add js

---
+++ index.js
@@ -0,0 +1,19 @@
+const label = document.getElementById("counter");
+const increase = document.getElementById("increase");
+const decrease = document.getElementById("decrease");
+const reset = document.getElementById("reset");
+
+count = 0
+
+increase.onclick = function(){
+    count++;
+    label.textContent = count;
+}
+decrease.onclick = function(){
+    count--;
+    label.textContent = count;
+}
+reset.onclick = function(){
+    count = 0;
+    label.textContent = count;
+}
